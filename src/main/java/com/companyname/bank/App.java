5af99b23b0cac35e7275ccbaa49add0181f777fc
prasad318 <131165740+prasad318@users.noreply.github.com>
2024-07-25 21:38:08 +0530

    update change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "code new change poll scm");
+        System.out.println( "code new change poll scm updated");
     }
 }
