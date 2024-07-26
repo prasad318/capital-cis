70e4ae3e66ce5cd5b6fdc73145bae6a36f7aab6c
prasad318 <131165740+prasad318@users.noreply.github.com>
2024-07-26 11:26:48 +0530

    poll scm changed

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "code new change poll scm updated");
+        System.out.println( "code new change poll scm updated and changed");
     }
 }
